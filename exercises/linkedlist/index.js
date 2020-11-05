8b857e5e81b6caeee7518e29a566bddd7c114ffa
Michael <michaeljanschiumo@gmail.com>
2020-11-04 20:46:50 -0500

    removeLast

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -60,6 +60,34 @@ class LinkedList {
 
     this.head = this.head.next;
   }
+
+  removeLast() {
+    //handle edge cases
+
+    //if the list is empty, return
+    if (!this.head) {
+      return;
+    }
+
+    //if the list only has one element, set head to null
+    if (!this.head.next) {
+      this.head = null;
+      return;
+    }
+
+    //set two pointers, prev and node
+    let prev = this.head;
+    let node = this.head.next;
+
+    //as long as node.next is not null, we progress the pointers
+    while (node.next) {
+      prev = node;
+      node = node.next;
+    }
+    //once the loop closes, we know that node.next is null
+    //we can simply set the next property of prev to null, making it the last element
+    prev.next = null;
+  }
 }
 
 module.exports = { Node, LinkedList };
